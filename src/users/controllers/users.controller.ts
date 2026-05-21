ebde9ad7ee99618b9fe20dedada544dd285b07c8
Juan Cruz <olguinjuacruz@gmail.com>
2026-05-21 13:49:46 -0300

    rol admin requerido en users.controller.ts

--- src/users/controllers/users.controller.ts
+++ src/users/controllers/users.controller.ts
@@ -1,9 +1,15 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Param, UseGuards } from '@nestjs/common';
 
 import { UsersService } from '../services/users.service';
 
 import { ExternalUser } from '../user.types';
+import { UserRol } from '../../auth/types/user-role.enum';
+import { JwtAuthGuard } from '../../auth/guards/jwt-auth.guard';
+import { RolesGuard } from '../../auth/guards/roles.guard';
+import { Roles } from '../../common/decorators/roles.decorator';
 
+@UseGuards(JwtAuthGuard, RolesGuard)
+@Roles(UserRol.ADMIN)
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
